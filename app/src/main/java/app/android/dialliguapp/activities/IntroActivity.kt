d6542294cdf9fe3bb0b9453e820f571d3db965b4
danielSajjadi <danial.sajadi@gmail.com>
2023-04-12 11:16:58 +0200

    change .git path

--- app/src/main/java/app/android/dialliguapp/activities/IntroActivity.kt
+++ app/src/main/java/app/android/dialliguapp/activities/IntroActivity.kt
@@ -2,11 +2,17 @@ package app.android.dialliguapp.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager.LayoutParams.FLAG_FULLSCREEN
 import app.android.dialliguapp.R
 
 class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
+
+        window.setFlags(
+            FLAG_FULLSCREEN,
+            FLAG_FULLSCREEN
+        )
     }
 }
