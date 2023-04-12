a718032cd757e279e7b72d704eecc47fb39447b1
danielSajjadi <danial.sajadi@gmail.com>
2023-04-12 08:19:29 +0200

    initial commit

---
+++ LinguApp/app/src/main/java/app/android/dialliguapp/activities/SplashActivity.kt
@@ -0,0 +1,30 @@
+package app.android.dialliguapp.activities
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager.LayoutParams.FLAG_FULLSCREEN
+import app.android.dialliguapp.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        // to make tha layout fullscreen
+        window.setFlags(
+            FLAG_FULLSCREEN,
+            FLAG_FULLSCREEN
+        )
+
+        // Intent -> pass data between app components.// Intents are a powerful tool for communication and data sharing between app components in Android
+        Handler().postDelayed(
+            {
+                startActivity(Intent(this, IntroActivity::class.java))
+                finish()
+            },
+            2500
+        )
+    }
+}
