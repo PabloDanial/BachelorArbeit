16569b31af4f1a06d1095fa6bd7ef362450eb245
danielSajjadi <danial.sajadi@gmail.com>
2023-04-12 17:08:47 +0200

    add instruction layout

---
+++ app/src/main/java/app/android/dialliguapp/activities/InstructionsActivity.kt
@@ -0,0 +1,12 @@
+package app.android.dialliguapp.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import app.android.dialliguapp.R
+
+class InstructionsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_instructions)
+    }
+}
--- app/src/main/java/app/android/dialliguapp/activities/IntroActivity.kt
+++ app/src/main/java/app/android/dialliguapp/activities/IntroActivity.kt
@@ -1,9 +1,14 @@
 package app.android.dialliguapp.activities
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.view.WindowManager.LayoutParams.FLAG_FULLSCREEN
+import android.widget.Button
+import android.widget.TextView
 import app.android.dialliguapp.R
+
 
 class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,5 +19,11 @@ class IntroActivity : AppCompatActivity() {
             FLAG_FULLSCREEN,
             FLAG_FULLSCREEN
         )
+
+        val myButton = findViewById<Button>(R.id.btn_start_game)
+        myButton.setOnClickListener {
+            startActivity(Intent(this,InstructionsActivity::class.java))
+        }
+
     }
 }
